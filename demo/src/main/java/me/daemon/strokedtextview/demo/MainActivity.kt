9bc362f8e2527ceaf61d729113345e8c35afd3b1
daemon369 <daemon336699@gmail.com>
2021-04-08 14:20:50 +0800

    migrate to androidx

--- demo/src/main/java/me/daemon/strokedtextview/demo/MainActivity.kt
+++ demo/src/main/java/me/daemon/strokedtextview/demo/MainActivity.kt
@@ -2,7 +2,7 @@ package me.daemon.strokedtextview.demo
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import me.daemon.strokedtextview.StrokedTextView
 
 class MainActivity : AppCompatActivity() {
