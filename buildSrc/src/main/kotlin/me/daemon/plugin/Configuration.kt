4b72ea2dc5918189f4d0aa1c4c2570bec093f37f
daemon369 <daemon336699@gmail.com>
2021-06-16 12:05:09 +0800

    add buildSrc and build scripts

---
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -0,0 +1,11 @@
+package me.daemon.plugin
+
+object Configuration {
+    const val minSdkVersion = 21
+    const val compileSdkVersion = 30
+    const val targetSdkVersion = 29
+    const val buildToolsVersion = "30.0.3"
+    const val groupId = "io.github.daemon369"
+    const val artifactId = "stroked-text-view"
+    const val version = "0.1.0"
+}
