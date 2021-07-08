bdd8f4721494d4b4feb865f75bac878a89e7f1d7
daemon369 <daemon336699@gmail.com>
2021-07-08 21:30:30 +0800

    change targetSdkVersion to 23

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -3,9 +3,9 @@ package me.daemon.plugin
 object Configuration {
     const val minSdkVersion = 21
     const val compileSdkVersion = 30
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 23
     const val buildToolsVersion = "30.0.3"
     const val groupId = "io.github.daemon369"
     const val artifactId = "stroked-text-view"
-    const val version = "0.1.0"
+    const val version = "0.2.0"
 }
